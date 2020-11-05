b0bb88646a9e79db0c28f4f99bc0d1003cd16545
Ford <1546811634@mil>
2020-11-04 18:36:39 -0700

    Add test case

---
+++ src/test/java/io/github/usafa_compsci350/BoardTest.java
@@ -0,0 +1,41 @@
+package io.github.usafa_compsci350;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class BoardTest {
+  @Test
+  public void printBoardTest() {
+    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+    //main only draws the board right now
+    new Board().drawBoard(new PrintStream(stream));
+    //Board.main(new PrintStream(stream));
+    String actual = stream.toString();
+    //expected string representation of board
+    String expected =
+        "8, ,8, ,8, ,8, ,8, ,8, ,8, ,8, ,8, ,8,\n" +
+            " , , , , , , , , ,1, , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , , , , , , , , , , , ,\n" +
+            " , , , , , , , , ,2, , , , , , , , , ,\n" +
+            "8, ,8, ,8, ,8, ,8, ,8, ,8, ,8, ,8, ,8,\n";
+    Assertions.assertEquals(expected, actual);
+  }
+}
